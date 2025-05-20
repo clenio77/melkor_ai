b9a587a382499d1d9b409ecb28ba3884e5ad8994
clenio <mouraaf@hotmail.com>
2025-05-19 22:07:49 -0300

    update start.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Instalar dependências Python
 pip install -r requirements.txt
+
+# Coletar arquivos estáticos
 python manage.py collectstatic --no-input
+
+# Aplicar migrações do banco de dados
 python manage.py migrate --no-input
+
 python manage.py create_superuser
+
 gunicorn melkor_project.wsgi:application
